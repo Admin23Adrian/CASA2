48c75c09ac4a27448c830fcb2c67b8acabea076e
Admin23Adrian <adrian23.alarcon@gmail.com>
2022-02-10 18:08:57 -0300

    Se termino la parte de buscar toda la informacion, ordenarla y pegarla en el excel. Mañana hacemos una pequeña muestra de todo el desarrollo integral.

--- main.py
+++ main.py
@@ -57,7 +57,6 @@ def limpiar_grupos(lista_medicacion, lista_cantidades):
     return lista, lista_final_cantidades, lista_codigo_troquel
 
 
-
 # Funcion que limpia los caracteres "\n" del texto del pdf:
 def limpiar_caracteres(texto_lista):
     if texto_lista != []:
@@ -68,21 +67,23 @@ def limpiar_caracteres(texto_lista):
         return False
 
 
-# Pegar datos a Excel. Parametro fl = (fila excel)
-def excel(ruta_excel, afiliado, medicacion_lista, cantidades, fl):
+# Pegar datos a Excel
+def excel(ruta_excel, afiliado, medicacion_lista, cantidades, cod_troquel):
+    global fila
     print(f"\t\t# INTENTANDO ACCEDER AL EXCEL...")
     excel = openpyxl.load_workbook(ruta_excel)
     hoja = excel["inicio"]
-    codigo_afiliado = afiliado[0][0] + afiliado[0][1]
     
+    codigo_afiliado = afiliado[0][0] + afiliado[0][1]
+    ultima_fila = len(hoja["B"])
+
     try:
         print(f"\t\t> Se accedio correctamente.")
-        posic = 0
         for i in range(len(medicacion_lista)):
-            posic = posic + fl
-            hoja[f"B{posic}"].value = codigo_afiliado
-            hoja[f"C{posic}"].value = medicacion_lista[i]
-            hoja[f"D{posic}"].value = cantidades[i]
+            hoja[f"B{fila}"].value = codigo_afiliado
+            hoja[f"C{fila}"].value = " ".join(medicacion_lista[i])
+            hoja[f"D{fila}"].value = " ".join(cantidades[i])
+            fila += 1
 
         # # Verificamos si el valor se pego correctamente en las celdas.
         # if hoja[f"B{fl}"].value != None:
@@ -94,14 +95,12 @@ def excel(ruta_excel, afiliado, medicacion_lista, cantidades, fl):
         #     print(f"\t\t> La medicacion {afiliado} se pego correctamente al Excel.")
         # else:
         #     print(f"\t\t> La medicacion {afiliado} NO SE HA PODIDO PEGAR EN EL EXCEL. Se deja LOG.")
-    except:
-        print("Ha ocurrido una excepcion. Posiblemente hay un error con el EXCEL.")
+    except Exception as e:
+        print(f"Ha ocurrido una excepcion. Posiblemente hay un error con el EXCEL. {e}")
     # Guardamos y cerramos el excel.
     finally:
         excel.save(ruta_excel)
         excel.close()
-
-    return posic
 
 
 # Leer pdf y sacar medicacion
@@ -141,7 +140,7 @@ def leer_pdf(ruta_pdfs):
     except Exception as e:
         return False, False
     
-    # # Validamos afiliado y medicacion.
+    # Validamos afiliado y medicacion.
     if lista_afiliado != []:
         return lista_afiliado, juego_tuplas_medicamentos, cantidades
     else:
@@ -152,7 +151,7 @@ def leer_pdf(ruta_pdfs):
 # --- ARRANQUE DEL PROCESO PRINCIPAL --- #
 if __name__ == "__main__":
     try:
-        f = 9
+        fila = 9
         # Se recorre la carpeta de los pdf y se envia la lectura.
         print(">>> Ingresando a la carpeta de PDFS.")
         for pdf in os.listdir(rutas.carpeta_pdfs):
@@ -160,16 +159,15 @@ if __name__ == "__main__":
             print("\tListando PDF:")
             if pdf.endswith(".pdf"):
                 print(f"\t\t* {pdf}")
-                lista_afiliado, lista_tuplas_medicacion, lista_cantidades = leer_pdf(os.path.join(rutas.carpeta_pdfs, pdf))
+                lista_afiliado, lista_tuplas_medicacion, tupla_cantidades = leer_pdf(os.path.join(rutas.carpeta_pdfs, pdf))
                 
-                
+                print(fila)
                 if lista_afiliado != False and lista_tuplas_medicacion != False:
-                    limpiar_grupos(lista_tuplas_medicacion, lista_cantidades)
-                    # cantidades_limpias, materiales_limpios, cantidades_limpias = limpiar_grupos(lista_tuplas_medicacion, lista_cantidades)
-                
+                    lista_materiales, lista_cantidades, lista_codigos_troquel = limpiar_grupos(lista_tuplas_medicacion, tupla_cantidades)
+                    # print(lista_materiales, lista_cantidades, lista_codigos_troquel)
+                    excel(rutas.archivo_excel, lista_afiliado, lista_materiales, lista_cantidades, lista_codigos_troquel)
                 else:
                     print(f"\t\t No se pudo encontrar medicacion para el PDF {pdf}")
-                #     
 
     except Exception as e:
         print("***** Nada que listar en la carpeta de PDFS! *****")
